c079d48847dbdc966f289bbb20d694c67ab30d21
krishnasankar <sankarkrishna689@gmail.com>
2024-05-14 13:35:07 +0530

    asdfas

--- src/components/CustomCursor.jsx
+++ src/components/CustomCursor.jsx
@@ -23,7 +23,7 @@ const CustomCursor = (props) => {
     className="fixed z-50 hidden md:block transition-all duration-75" 
     style={{ left: position.x, top: position.y }}
   >
-    <div className={`pointer-events-none ${props.theme ? 'bg-blue' : 'bg-green'} ${props.mousePointer ? 'pointer' : ''} ${props.theme ? 'border-blue' : 'border-green'} absolute transition-all duration-1000 w-4 h-4 rounded-full rotate-12`} ></div>
+    <div className={`pointer-events-none ${props.theme ? 'bg-blue' : 'bg-green'} ${props.mousePointer ? '' : ''} ${props.theme ? 'border-blue' : 'border-green'} absolute transition-all duration-1000 w-4 h-4 rounded-full rotate-12`} ></div>
   </div>
   );
 };
